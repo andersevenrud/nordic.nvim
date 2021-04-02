c7babba830bbf4bc32eba6434ebb096ca89df4a8
Maas Lalani <maaslalani0@gmail.com>
2021-04-02 00:51:02 -0400

    Update lua/nordbuddy.lua

--- lua/nordbuddy.lua
+++ lua/nordbuddy.lua
@@ -209,7 +209,7 @@ function M:diff()
         {'GitGutterDelete', c.nord11, c.nord1}, --
         {'NeogitDiffAddHighlight', c.nord14, c.nord1}, --
         {'NeogitDiffDeleteHighlight', c.nord11, c.nord1}, --
-        {'NeogitDiffContextHighlight', c.nord13, c.nord1}, --
+        {'NeogitDiffContextHighlight', c.nord13, c.nord0}, --
         {'NeogitHunkHeaderHighlight', c.nord3:light(), c.nord0}, --
         {'NeogitHunkHeader', c.nord3:light(), c.nord0} --
     }
