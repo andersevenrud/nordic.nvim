723c39690f59afba3fca006563f6429bf25328f6
Maas Lalani <maaslalani0@gmail.com>
2021-05-30 00:41:56 -0400

    add: vimwiki h1-h3 highlighting

---
+++ lua/nordbuddy/colors/vimwiki.lua
@@ -0,0 +1,8 @@
+return function(c, s, cs)
+    -- 'vimwiki/vimwiki'
+    return {
+        {'VimwikiHeader1', c.nord8},
+        {'VimwikiHeader2', c.nord9},
+        {'VimwikiHeader3', c.nord15},
+    }
+end
