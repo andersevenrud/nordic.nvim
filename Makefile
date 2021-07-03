b67e778d59ce566ca6b46e19828917ed8546987a
Anders Evenrud <andersevenrud@gmail.com>
2021-07-04 00:21:01 +0200

    Remove unused tests (#21)

--- Makefile
+++
@@ -1,2 +0,0 @@
-test:
-	nvim --headless -c 'PlenaryBustedDirectory tests/'
--- tests/nordbuddy_spec.lua
+++
@@ -1,8 +0,0 @@
-local nordbuddy = require'nordbuddy'
-
-describe('colorscheme', function()
-  it("should not error", function()
-    nordbuddy.setup()
-    assert.has_no.errors(nordbuddy.colors)
-  end)
-end)
