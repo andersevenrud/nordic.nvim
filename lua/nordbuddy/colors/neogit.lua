3a5002ee52c034d98e49e680392a94c0a53295e1
Maas Lalani <maaslalani0@gmail.com>
2021-07-12 16:11:59 -0400

    neogit tweaks

--- lua/nordbuddy/colors/neogit.lua
+++ lua/nordbuddy/colors/neogit.lua
@@ -3,7 +3,7 @@ return function(c)
     return {
         {'NeogitDiffAddHighlight', c.green, c.black},
         {'NeogitDiffDeleteHighlight', c.red, c.black},
-        {'NeogitDiffContextHighlight', c.yellow, c.dark_black},
+        {'NeogitDiffContextHighlight', c.white, c.dark_black},
         {'NeogitHunkHeaderHighlight', c.grayish, c.dark_black},
         {'NeogitHunkHeader', c.grayish, c.dark_black}
     }
