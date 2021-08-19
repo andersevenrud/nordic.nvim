095bfe850df9d37c2ffb6b3573b417782eb94b61
Anders Evenrud <andersevenrud@gmail.com>
2021-08-19 15:50:16 +0200

    Add missing diff style
    Closes #39

--- lua/nordbuddy/colors/diff.lua
+++ lua/nordbuddy/colors/diff.lua
@@ -1,6 +1,7 @@
 return function(c)
     return {
         {'DiffAdd', c.green, c.black},
+        {'DiffAdded', c.green, c.black},
         {'DiffChange', c.yellow, c.black},
         {'DiffDelete', c.red, c.black},
         {'DiffText', c.red, c.black}
