a5fe7345b2f2d3e5db680d919554e4384826c96f
Dan Sully <dan+github@sully.org>
2021-09-18 11:06:33 -0700

    Update for new Diagnostic highlights.

--- lua/nordbuddy/colors/lsp.lua
+++ lua/nordbuddy/colors/lsp.lua
@@ -6,6 +6,12 @@ return function(c)
         {'LspDiagnosticsDefaultHint', c.intense_blue},
         {'LspDiagnosticsDefaultError', c.red},
         {'LspDiagnosticsDefaultWarning', c.yellow},
-        {'LspDiagnosticsDefaultInformation', c.bright_cyan}
+        {'LspDiagnosticsDefaultInformation', c.bright_cyan},
+
+        -- Support for updated highlights - https://github.com/neovim/neovim/pull/15585
+        {'DiagnosticError', c.red},
+        {'DiagnosticHint', c.intense_blue},
+        {'DiagnosticInfo', c.bright_cyan},
+        {'DiagnosticWarn', c.yellow},
     }
 end
