9961fe0a0afd18da168f299ee6c1ab52040f9c42
Maas Lalani <maaslalani0@gmail.com>
2021-09-25 14:08:41 -0400

    cmp colors

--- lua/nordbuddy/colors/cmp.lua
+++ lua/nordbuddy/colors/cmp.lua
@@ -3,10 +3,10 @@ return function(c)
   return {
     {'CmpDocumentation', c.white, c.none },
     {'CmpDocumentationBorder', c.grayish, c.none },
-    {'CmpItemAbbr', c.grayish, c.none },
-    {'CmpItemAbbrMatch', c.cyan, c.none },
-    {'CmpItemAbbrMatchFuzzy', c.cyan, c.none },
-    {'CmpItemKind', c.white, c.none },
-    {'CmpItemMenu', c.none, c.bright_black }
+    {'CmpItemAbbr', c.dark_white, c.none },
+    {'CmpItemAbbrMatch', c.bright_cyan, c.none },
+    {'CmpItemAbbrMatchFuzzy', c.bright_cyan, c.none },
+    {'CmpItemKind', c.grayish, c.none },
+    {'CmpItemMenu', c.none, c.black }
   }
 end
