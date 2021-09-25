781a79ffe8728f0ac35dd95a5fa756f4e5346d2a
Maas Lalani <maaslalani0@gmail.com>
2021-09-25 13:52:26 -0400

    Cmp

--- lua/nordbuddy/colors/cmp.lua
+++ lua/nordbuddy/colors/cmp.lua
@@ -1,7 +1,12 @@
 -- 'hrsh7th/nvim-cmp'
 return function(c)
   return {
-    {'CmpDocumentation', c.white, c.none},
-    {'CmpDocumentationBorder', c.grayish, c.none}
+    {'CmpDocumentation', c.white, c.none },
+    {'CmpDocumentationBorder', c.grayish, c.none },
+    {'CmpItemAbbr', c.grayish, c.none },
+    {'CmpItemAbbrMatch', c.cyan, c.none },
+    {'CmpItemAbbrMatchFuzzy', c.cyan, c.none },
+    {'CmpItemKind', c.white, c.none },
+    {'CmpItemMenu', c.none, c.bright_black }
   }
 end
