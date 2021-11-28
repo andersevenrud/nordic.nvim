e3942712a5fd3eebdd58594c4ce58dd2126510d4
Anders Evenrud <andersevenrud@gmail.com>
2021-11-28 23:19:36 +0100

    Add nvim-cmp completion kind colors

--- lua/nordbuddy/colors/cmp.lua
+++ lua/nordbuddy/colors/cmp.lua
@@ -1,12 +1,19 @@
 -- 'hrsh7th/nvim-cmp'
 return function(c)
   return {
-    {'CmpDocumentation', c.white, c.none },
-    {'CmpDocumentationBorder', c.grayish, c.none },
-    {'CmpItemAbbr', c.dark_white, c.none },
-    {'CmpItemAbbrMatch', c.bright_cyan, c.none },
-    {'CmpItemAbbrMatchFuzzy', c.bright_cyan, c.none },
-    {'CmpItemKind', c.grayish, c.none },
-    {'CmpItemMenu', c.none, c.black }
+    {'CmpDocumentation', c.white, c.none},
+    {'CmpDocumentationBorder', c.grayish, c.none},
+    {'CmpItemAbbr', c.dark_white, c.none},
+    {'CmpItemAbbrMatch', c.bright_cyan, c.none},
+    {'CmpItemAbbrMatchFuzzy', c.bright_cyan, c.none},
+    {'CmpItemKind', c.grayish, c.none},
+    {'CmpItemMenu', c.none, c.black},
+    {'CmpItemKindDefault', c.yellow, c.none},
+    {'CmpItemAbbrMatch', c.blue, c.none},
+    {'CmpItemAbbrMatchFuzzy', c.blue, c.none},
+    {'CmpItemKindFunction', c.purple, c.none},
+    {'CmpItemKindMethod', c.purple, c.none},
+    {'CmpItemKindVariable', c.cyan, c.none},
+    {'CmpItemKindKeyword', c.white, c.none},
   }
 end
