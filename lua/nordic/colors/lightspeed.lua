a1c9285a87a1c587a33fa7d8a196658f3f6eeee5
9seconds <nineseconds@yandex.ru>
2021-12-11 06:49:01 +0000

    Improve contrast and intensity of lightspeed.nvim colors

--- lua/nordic/colors/lightspeed.lua
+++ lua/nordic/colors/lightspeed.lua
@@ -1,12 +1,12 @@
 -- 'ggandor/lightspeed.nvim'
 return function(c, s)
     return {
-        { 'LightspeedLabel', c.blue, c.none, s.bold },
-        { 'LightspeedLabelOverlapped', c.blue, c.dark_white, s.reverse },
-        { 'LightspeedLabelDistant', c.purple, c.none, s.bold },
-        { 'LightspeedLabelDistantOverlapped', c.purple, c.none, s.reverse },
-        { 'LightspeedShortcut', c.intense_blue, c.black, s.bold },
-        { 'LightspeedShortcutOverlapped', c.intense_blue, c.none, s.reverse },
+        { 'LightspeedLabel', c.yellow, c.none },
+        { 'LightspeedLabelOverlapped', c.yellow, c.black, s.reverse },
+        { 'LightspeedLabelDistant', c.purple, c.bright_black, s.bold },
+        { 'LightspeedLabelDistantOverlapped', c.purple, c.bright_black, s.reverse },
+        { 'LightspeedShortcut', c.orange, c.none, s.bold },
+        { 'LightspeedShortcutOverlapped', c.orange, c.black, s.reverse },
         { 'LightspeedMaskedChar', c.dark_white, c.bright_black, s.bold },
         { 'LightspeedGreyWash', c.grayish },
         { 'LightspeedUnlabeledMatch', c.bright_white, c.bright_black },
