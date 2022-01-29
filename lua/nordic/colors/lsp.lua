6d0ccdb4d7e54b7ff29a626378f3c258b237d7db
Anders Evenrud <andersevenrud@gmail.com>
2022-01-29 13:37:24 +0100

    feat: add diagnostic underline color definitions (#67)

--- lua/nordic/colors/lsp.lua
+++ lua/nordic/colors/lsp.lua
@@ -1,4 +1,4 @@
-return function(c)
+return function(c, s)
     return {
         { 'LspReferenceText', c.dark_white },
         { 'LspReferenceRead', c.white },
@@ -7,6 +7,11 @@ return function(c)
         { 'LspDiagnosticsDefaultError', c.red },
         { 'LspDiagnosticsDefaultWarning', c.yellow },
         { 'LspDiagnosticsDefaultInformation', c.bright_cyan },
+
+        { 'DiagnosticUnderlineError', c.none, c.none, s.underline, c.red },
+        { 'DiagnosticUnderlineWarn', c.none, c.none, s.underline, c.yellow },
+        { 'DiagnosticUnderlineInfo', c.none, c.none, s.underline, c.bright_cyan },
+        { 'DiagnosticUnderlineHint', c.none, c.none, s.underline, c.intense_blue },
 
         -- Support for updated highlights - https://github.com/neovim/neovim/pull/15585
         { 'DiagnosticError', c.red },
