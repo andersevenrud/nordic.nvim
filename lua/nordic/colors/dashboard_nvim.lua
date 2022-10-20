4b8aaa8a8764b7dca233356dfcf70d686748741e
Thierry Delafontaine <deaa@zhaw.ch>
2022-10-20 08:58:51 +0200

    feat: hl groups for 'glepnir/dashboard-nvim'

---
+++ lua/nordic/colors/dashboard_nvim.lua
@@ -0,0 +1,9 @@
+-- 'glepnir/dashboard-nvim'
+return function(c)
+    return {
+        { 'DashboardHeader', c.purple },
+        { 'DashboardCenter', c.dark_white },
+        { 'DashboardShortCut', c.blue },
+        { 'DashboardFooter', c.gray },
+    }
+end
